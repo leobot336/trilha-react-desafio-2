a2d39a855b7ed99f51f3474f1e86a81fd63ac074
leobot336 <114504872+leobot336@users.noreply.github.com>
2024-11-09 15:45:53 -0300

    Update App.js

--- src/pages/App.js
+++ src/pages/App.js
@@ -35,8 +35,7 @@ function App() {
 
   const handleRemoveRepo = (id) => {
     console.log('Removendo registro', id);
-
-    // utilizar filter.
+    setRepos(repos.filter((repo)=>repo.id !== id))
   }
 
 
